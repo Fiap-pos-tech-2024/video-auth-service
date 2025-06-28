663771a0512a2bc1e837aae18493af0840811a4d
Erik <erik.fernandes87@gmail.com>
2025-06-28 15:46:10 -0300

    deploy alloy on cloud

--- src/config/prometheus.js
+++ src/config/prometheus.js
@@ -3,26 +3,27 @@ const client = require("prom-client");
 const register = new client.Registry();
 client.collectDefaultMetrics({ register });
 
+// 🔧 Altere os labels para: method, route, status_code
 const httpRequestDurationSeconds = new client.Histogram({
   name: "http_request_duration_seconds",
   help: "Duration of HTTP requests in seconds",
-  labelNames: ["method", "route", "code"],
+  labelNames: ["method", "route", "status_code"],
   buckets: [0.1, 0.5, 1, 1.5, 2, 5],
 });
 
 register.registerMetric(httpRequestDurationSeconds);
 
 const metricsMiddleware = (req, res, next) => {
-  if (req.path === "/metrics") {
-    return next();
-  }
+  if (req.path === "/metrics") return next();
 
   const end = httpRequestDurationSeconds.startTimer();
+
   res.on("finish", () => {
+    const route = req.route?.path || req.path || "unknown_route";
     end({
       method: req.method,
-      route: req.route ? req.route.path : req.path,
-      code: res.statusCode,
+      route,
+      status_code: res.statusCode,
     });
   });
 
